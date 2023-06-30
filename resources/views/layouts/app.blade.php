f3cb2dcfddb4041c1a7a0ed1388837de4f454663
Radhames2418 <Radhamesenc2412002@gmail.com>
2023-06-30 17:37:04 -0400

    "Implement delete function with confirmation in job vacancies view"
    In this update, we've integrated the functionality that allows users to delete job vacancies directly from the vacancies view. A confirmation dialog has been added through SweetAlert2 to ensure the users intentionally want to delete a vacante. This decision to include a deletion function in the view was prompted by the need for an easier and faster way to manage vacancies. Additional scripts and styles are pushed to the layouts template to support these changes.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,11 +11,14 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         <!-- Livewire style -->
         @livewireStyles
+
+        @stack('style');
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
@@ -37,5 +40,6 @@
         </div>
             <!-- Livewire Script  -->
             @livewireScripts
+            @stack('scripts');
     </body>
 </html>
--- resources/views/livewire/mostrar-vacantes.blade.php
+++ resources/views/livewire/mostrar-vacantes.blade.php
@@ -1,42 +1,79 @@
-<div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-    @forelse($vacantes as $vacante)
-        <div class="p-6 text-gray-900 border-b dark:text-gray-100 md:flex md:items-center md:justify-between">
-            <div class="leading-10">
-                <a class="text-xl font-bold" href="">
-                    {{ $vacante->titulo }}
-                </a>
-                <p class="text-sm text-gray-600 font-bold">
-                    {{ $vacante->empresa }}
-                </p>
+@push('styles')
+    <link rel="stylesheet" href="@sweetalert2/themes/dark/dark.css">
+@endpush
 
-                <p class="text-sm text-gray-500 my-2">último dia: {{ $vacante->ultimo_dia->format('d/m/y') }}</p>
+<div>
+    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+        @forelse($vacantes as $vacante)
+            <div class="p-6 text-gray-900 border-b dark:text-gray-100 md:flex md:items-center md:justify-between">
+                <div class="leading-10">
+                    <a class="text-xl font-bold" href="">
+                        {{ $vacante->titulo }}
+                    </a>
+                    <p class="text-sm text-gray-600 font-bold">
+                        {{ $vacante->empresa }}
+                    </p>
+
+                    <p class="text-sm text-gray-500 my-2">último dia: {{ $vacante->ultimo_dia->format('d/m/y') }}</p>
+                </div>
+
+                <div class="flex flex-col items-stretch md:flex-row md:items-center gap-3 mt-5 md:mt-0">
+                    <a
+                            href="#"
+                            class="bg-slate-800 py-2 px-4 rounded-lg text-white text-sm  font-bold text-center">
+                        Candidatos
+                    </a>
+
+                    <a
+                            href="{{ route('vacantes.edit', $vacante) }}"
+                            class="bg-blue-800 py-2 px-4 rounded-lg text-white text-sm  font-bold text-center">
+                        Editar
+                    </a>
+
+                    <button
+                            wire:click="$emit('eliminarVacante', {{ $vacante->id }})"
+                            class="bg-red-600 py-2 px-4 rounded-lg text-white text-sm  font-bold text-center">
+                        Eliminar
+                    </button>
+                </div>
             </div>
+        @empty
+            <p class="p-3 text-center text-sm text-gray-600">No hay vacantes</p>
+        @endforelse
+    </div>
 
-            <div class="flex flex-col items-stretch md:flex-row md:items-center gap-3 mt-5 md:mt-0">
-                <a
-                    href="#"
-                    class="bg-slate-800 py-2 px-4 rounded-lg text-white text-sm  font-bold text-center">
-                    Candidatos
-                </a>
+    <div class="flex justify-center  mt-10">
+        {{ $vacantes->links() }}
+    </div>
 
-                <a
-                    href="{{ route('vacantes.edit', $vacante) }}"
-                    class="bg-blue-800 py-2 px-4 rounded-lg text-white text-sm  font-bold text-center">
-                    Editar
-                </a>
-
-                <a
-                    href="#"
-                    class="bg-red-600 py-2 px-4 rounded-lg text-white text-sm  font-bold text-center">
-                    Eliminar
-                </a>
-            </div>
-        </div>
-    @empty
-        <p class="p-3 text-center text-sm text-gray-600">No hay vacantes</p>
-    @endforelse
 </div>
 
-<div class="flex justify-center  mt-10">
-    {{ $vacantes->links() }}
-</div>
+@push('scripts')
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script>
+        Livewire.on('eliminarVacante', function (VacanteId) {
+            Swal.fire({
+                title: 'Estas seguro?',
+                text: "No se podra revertir los cambios!",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Si, eliminalo!'
+            }).then((result) => {
+                if (result.isConfirmed) {
+
+                    // Eliminar la vacante del servidor
+
+                    Livewire.emit('eliminarVacante', VacanteId);
+
+                    Swal.fire(
+                        'Vacante eliminada!',
+                        'Eliminado correctamente',
+                        'OK'
+                    )
+                }
+            })
+        })
+    </script>
+@endpush
