f3b99622fd877f2514dddb8a6b8de67212115566
Radhames2418 <Radhamesenc2412002@gmail.com>
2023-07-08 23:33:13 -0400

    You only fail when you stop trying👑👑👑

--- app/Http/Livewire/HomeVacante.php
+++ app/Http/Livewire/HomeVacante.php
@@ -21,13 +21,23 @@ class HomeVacante extends Component
         $this->termino   = $termino;
         $this->categoria = $categoria;
         $this->salario   = $salario;
-        dd('desde componente padre');
     }
 
 
     public function render()
     {
-        $vacantes = Vacante::all();
+
+        $vacantes = Vacante::when($this->termino, function ($query) {
+            $query->where('titulo', 'LIKE', "%" . $this->termino . "%");
+        })
+            ->when($this->categoria, function ($query) {
+                $query->where('categoria_id', $this->categoria);
+            })
+            ->when($this->salario, function ($query) {
+                $query->where('salario_id', $this->salario);
+            })
+            ->paginate(20);
+
 
         return view('livewire.home-vacante', [
             'vacantes' => $vacantes
